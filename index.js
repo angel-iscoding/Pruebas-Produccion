a03db0f0138b6a5569ce38d3c04461d7d241a5ea
angel-iscoding <angel.iscoding@gmail.com>
2025-09-02 15:32:21 -0500

    6

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3010;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASS;
 const DB_DATABASE = process.env.DB_NAME;
-const DB_CONNECTION_NAME = process.env.DB_CONNECTION_NAME;
+/* const DB_CONNECTION_NAME = process.env.DB_CONNECTION_NAME;
 const socketPath = `/cloudsql/${DB_CONNECTION_NAME}`;
 
 // Configuración de la conexión a Cloud SQL
@@ -30,8 +30,25 @@ app.get('/health', async (req, res) => {
     console.error('Database connection failed:', err);
     res.status(500).send('Database connection failed');
   }
+}); */
+
+app.get('/health', async (req, res) => {
+  try {
+    res.status(200).send('Servidor y base de datos conectados.');
+  } catch (err) {
+    console.error('Database connection failed:', err);
+    res.status(500).send('Database connection failed');
+  }
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Environment loaded:
+                PORT: ${PORT}
+                
+                Database environment variables:
+
+                DB_USER: ${DB_USER}
+                DB_PASSWORD: ${DB_PASSWORD}
+                DB_DATABASE: ${DB_DATABASE}
+                `);
 });
