de84deb92005479ccbcc6eb74ae1de9f44f9ba22
angel-iscoding <angel.iscoding@gmail.com>
2025-09-02 14:53:41 -0500

    Tercer commit

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 import express from 'express';
+import mysql from 'mysql2'; // Importar la librería
 import dotenv from 'dotenv';
 
-const app = express();
 dotenv.config();
+const app = express();
 
-const PORT = process.env.PORT || 3010; 
-const DB_USER = process.env.DB_USER || "roomiez";
-const DB_PASSWORD = process.env.DB_PASSWORD || "roomiezpass";
-const DB_DATABASE = process.env.DB_DATABASE || "RoomieZ";
+const PORT = process.env.PORT || 3010;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASS;
+const DB_DATABASE = process.env.DB_NAME;
+const DB_CONNECTION_NAME = process.env.DB_CONNECTION_NAME;
+const socketPath = `/cloudsql/${DB_CONNECTION_NAME}`;
 
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Environment loaded:
-                PORT: ${PORT}
-                
-                Database environment variables:
-
-                DB_USER: ${DB_USER}
-                DB_PASSWORD: ${DB_PASSWORD}
-                DB_DATABASE: ${DB_DATABASE}
-
-                Google Cloud SQL Configuration:
-
-                Socket Path: ${socketPath}
-                `);
+// Configuración de la conexión a Cloud SQL
+const pool = mysql.createPool({
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  socketPath: socketPath
 });
 
-app.get('/', (req, res) => {
-    res.send('Servidor iniciado!');
-})
+// Lógica para verificar la conexión o manejar peticiones
+app.get('/health', (req, res) => {
+  // Intenta obtener una conexión para verificar que todo está bien
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Database connection failed:', err);
+      return res.status(500).send('Database connection failed');
+    }
+    connection.release();
+    res.status(200).send('Servidor y base de datos conectados.');
+  });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
+});
